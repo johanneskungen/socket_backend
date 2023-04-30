7c96b2b77be116833760572a42db25027c8382cd
Johannes Eriksson <johannes.einar.eriksson@gmail.com>
2023-04-30 13:01:04 +0200

    changing url

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server({
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://socket-frontend-ochre.vercel.app/",
   },
 });
 
